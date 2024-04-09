c81ca2edc21b2afe92b5b2b169601668b892dfd0
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2024-04-09 21:18:51 +0100

    fix: Remove the whitespaces between parameters.

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,28 +2,13 @@
 #include <iostream>
 #include <stdint.h>
 
-void	putstr(char *s)
-{
-	std::string str(s);
-	for (uint32_t i = 0; i < str.length(); i++)
-		str[i] = std::toupper(str[i]);
-	std::cout << str;
-}
-
 int	main(int ac, char **av)
 {
-	(void)av;
 	if (ac == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
-	}
-	for (uint32_t i = 1; i < (uint32_t)ac; i++)
-	{
-		putstr(av[i]);
-		if (i < (uint32_t)ac - 1)
-			std::cout << " ";
-	}
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+		for (uint32_t i = 1; av[i]; i++)
+			for (uint32_t j = 0; av[i][j] ; j++)
+				std::cout << (char)std::toupper(av[i][j]);
 	std::cout << std::endl;
-	return (0);
 }
